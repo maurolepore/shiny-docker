6890bbff68e7d07eedc732ea0ddfa3fad46c240f
Mauro Lepore <maurolepore@gmail.com>
2024-10-29 15:49:04 +0000

    Update app.R

--- src/app.R
+++ src/app.R
@@ -43,7 +43,7 @@ server <- function(input, output) {
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white',
              xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+             main = 'HISTOGRAM OF WAITING TIMES')
     })
 }
 
